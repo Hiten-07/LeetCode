77f6537ea8f939dbdce2b556fb953f9c4dc77a1c
Hiten Senva <hitensenva44@gmail.com>
2026-07-02 22:01:36 +0530

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -15,3 +15,23 @@ class Solution {
         return {};
     }
 };
+
+//optimized code with hash map
+
+class Solution {
+public:
+
+vector<int>twoSum(vector<int>&nums, int target) {
+    unordered_map<int,int>map;
+
+for(int i=0; i<nums.size(); i++) {
+    int complement = target - nums[i];
+
+    if(map.find(complement) != map.end()) {
+        return {map[complement], i};
+    }
+    map[nums[i]] = i;
+}
+    return {};
+    }   
+}
