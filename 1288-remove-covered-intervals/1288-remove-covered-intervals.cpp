b72a218974000043cc5d422056686680cbafb21c
Hiten Senva <hitensenva44@gmail.com>
2026-07-06 21:20:22 +0530

    Time: 0 ms (100%), Space: 15 MB (82.12%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto &a, auto &b) {
+            if (a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
+        });
+
+        int count = 0, maxRight = -1;
+        for (auto &iv : intervals) {
+            if (iv[1] > maxRight) {
+                count++;
+                maxRight = iv[1];
+            }
+            // else covered, skip
+        }
+        return count;
+    }
+};
